a0e3011b1179023457213afbb4cdf046b5c43054
rpiontik <r.piontik@mail.ru>
2024-06-13 20:01:56 +0300

    Исправлено вечное колесо при изменении проекта

--- src/global/manifest/parser2.mjs
+++ src/global/manifest/parser2.mjs
@@ -553,20 +553,26 @@ parser.onChange = async function(sources) {
         if (layer.transaction === this.transaction) continue;
         // Если слой входит в список изменений - перезагружаем его
         if (sources.indexOf(layer.uri) >= 0) {
+            // eslint-disable-next-line no-console
+            console.info('>>>>>> Found layer for ', layer.uri);
+            isAffected = true;
             try {
                 await layer.reload(layer.uri);
-                if (!isAffected && this.onStartReload) this.onStartReload();
             } catch (e) {
                 this.registerError(e, e?.uri || layer.uri);
             }
-            isAffected = true;
         }
     }
     // Если в данных есть изменения - перестраиваем слои
     if (isAffected) {
+        // Вызываем слушателя начала обновления данных в манифесте
+        this.onStartReload && this.onStartReload();
         parser.rebuildLayers();
-        // Вызываем слушателя обновления данных в манифесте
+        // Вызываем слушателя окончания обновления данных в манифесте
         this.onReloaded && this.onReloaded(this);
+    } else {
+        // eslint-disable-next-line no-console
+        console.info('>>>>>> No found layer for ', sources);
     }
 };
 
