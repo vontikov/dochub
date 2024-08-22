6c8ee2bb7406f45fc8935b817d6dd68ea5e616de
rpiontik <r.piontik@mail.ru>
2024-08-22 20:40:25 +0300

    Добавлена возможность конфигурации интеграции с gitlab и bitbucket

--- src/frontend/components/settings/main.vue
+++ src/frontend/components/settings/main.vue
@@ -2,7 +2,7 @@
   <div>
     <v-container fluid class="d-flex justify-start flex-wrap">
       <v-flex v-for="(item, index) in pages" v-bind:key="index" md6 sm12 class="setting-item">
-        <div style="width: 100%;">
+        <div class="setting-content">
           <h2 sm12>{{ getTitle(item.location) }}</h2>
           <component v-bind:is="item.component" sm12 />
         </div>
@@ -34,7 +34,15 @@
 <style scoped>
 
 .setting-item {
-    padding: 8px;
+    
+}
+
+
+.setting-content {
+  width: calc(100% - 8px);
+  box-shadow: rgba(0, 0, 0, 0.2) 0px 3px 1px -2px, rgba(0, 0, 0, 0.14) 0px 2px 2px 0px, rgba(0, 0, 0, 0.12) 0px 1px 5px 0px;
+  margin: 4px;
+  padding: 8px;
 }
 
 </style>
