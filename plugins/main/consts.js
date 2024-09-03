ed570803201acea045671a067e754da1f60f170b
rpiontik <r.piontik@mail.ru>
2024-09-03 10:28:50 +0300

    Причесан Bitbucket драйвер под TS

--- plugins/main/consts.js
+++ plugins/main/consts.js
@@ -1,4 +1,6 @@
 export default {
     // Адрес универсального сервера авторизации Dochub
-    REGISTRY_SERVER: process.env.VUE_APP_DOCHUB_DEV_AUTH_SERVICE || 'https://registry.dochub.info'
+    REGISTRY_SERVER: process.env.VUE_APP_DOCHUB_DEV_AUTH_SERVICE || 'https://registry.dochub.info',
+    // Адрес публичного API Bitbucket 
+    PUBLUC_API_SERVER: 'https://api.bitbucket.org/2.0/'
 };
